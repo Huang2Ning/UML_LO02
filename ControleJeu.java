3b0bc3dca6dfe13eea7b1d2401e246ec2491c4e0
Huang2Ning <33783614+Huang2Ning@users.noreply.github.com>
2017-11-20 14:16:33 +0100

    Update ControleJeu.java
    voila ce que j'ai fait jusqu'au maintenant.

--- ControleJeu.java
+++ ControleJeu.java
@@ -1,7 +1,29 @@
 package crazyEight;
+//import source pour afficher l'heure
+import java.util.Date; 
 
 public class ControleJeu {
+	private static Date tempsDeJeu;
+	//la sens l'horaire par default (false: sens horaire, true: sens antihoraire)
+	private boolean sensPartie=false;
+	private JoueurVirtuel joueurEnCours;
+	
+	public boolean isSensPartie() {
+		return sensPartie;
+	}
 
+	public void setSensPartie(boolean sensPartie) {
+		this.sensPartie = sensPartie;
+	}
 	
-	
+	public JoueurVirtuel afficherJEnCours() {
+		joueurEnCours=joueur
+		return joueurEnCours;
+	}
+
+	/*public static void main(String[] args) {
+		Date date = new Date();
+		tempsDeJeu=date;
+		System.out.print(tempsDeJeu);
+	}*/
 }
